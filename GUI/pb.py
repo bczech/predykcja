4e89a6561cd6e6bdf366570e1042c4fd3a32e9ac
Dziabaku <33513863+dziabaku@users.noreply.github.com>
2018-01-22 16:57:09 +0100

    version update
    version: 1.0
    
    Help added.
    Few small mistakes repaired. Program ready to pack into installer.

--- GUI/pb.py
+++ GUI/pb.py
@@ -8,7 +8,7 @@ h2val = 0.64
 datasi = Calc().datasire(data, data2)
 ranking = Calc().rankingchange(h2val, datasi[0], datasi[1], datasi[2])
 pearson = Calc().Pearson(ranking[1], ranking[2])
-#plot = Calc().makeplot(ranking[1], ranking[2])
+plot = Calc().makeplot(ranking[1], ranking[2])
 
 print(pearson)
 
--- GUI/readme.py
+++ GUI/readme.py
@@ -3,6 +3,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
 
+
 class PredoBreedInfo(QWidget):
     def __init__(self):
         super().__init__()
